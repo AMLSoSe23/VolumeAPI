b41d63c87b06802a14f75bd96dc0cc4f201a8be2
Maximilian Graf <maxgraf27@gmail.com>
2023-07-02 17:57:05 +0100

    created density lookup as separate module

--- food_volume_estimation/volume_estimator.py
+++ food_volume_estimation/volume_estimator.py
@@ -12,57 +12,10 @@ from fuzzywuzzy import fuzz, process
 import matplotlib.pyplot as plt
 from food_volume_estimation.depth_estimation.custom_modules import *
 from food_volume_estimation.depth_estimation.project import *
+from food_volume_estimation.density_lookup.density_database import DensityDatabase
 from food_volume_estimation.food_segmentation.food_segmentator import FoodSegmentator
 from food_volume_estimation.ellipse_detection.ellipse_detector import EllipseDetector
 from food_volume_estimation.point_cloud_utils import *
-
-
-class DensityDatabase():
-    """Density Database searcher object. Food types are expected to be
-    in column 1, food densities in column 2."""
-    def __init__(self, db_path):
-        """Load food density database from file or Google Sheets ID.
-
-        Inputs:
-            db_path: Path to database excel file (.xlsx) or Google Sheets ID.
-        """
-        if os.path.exists(db_path):
-            # Read density database from excel file
-            self.density_database = pd.read_excel(
-                db_path, sheet_name=0, usecols=[0, 1])
-        else:
-            # Read density database from Google Sheets URL
-            sheet = 'Sheet1'
-            url = 'https://docs.google.com/spreadsheets/d/{0}/gviz/tq?tqx=out:csv&sheet={1}'.format(
-                db_path, sheet)
-            self.density_database = pd.read_csv(url, usecols=[0, 1],
-                                                header=None)
-        # Remove rows with NaN values
-        self.density_database.dropna(inplace=True)
-
-    def query(self, food):
-        """Search for food density in database.
-
-        Inputs:
-            food: Food type to search for.
-
-        Returns:
-            db_entry_vals: Array containing the matched food type
-            and its density.
-        """
-        try:
-            # Search for matching food in database
-            match = process.extractOne(food, self.density_database.values[:,0],
-                                       scorer=fuzz.partial_ratio,
-                                       score_cutoff=80)
-            db_entry = (
-                self.density_database.loc[
-                self.density_database[
-                self.density_database.columns[0]] == match[0]])
-            db_entry_vals = db_entry.values
-            return db_entry_vals[0]
-        except:
-            return ['None', 1]
 
 
 class VolumeEstimator():
